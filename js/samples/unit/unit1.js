a939758d0c981d3d603070ad837cb9da2296c954
jamin <gerard.jamin@orange.fr>
2024-01-13 17:14:35 +0100

    ajout de script dans package.json

--- js/samples/unit/unit1.js
+++ js/samples/unit/unit1.js
@@ -2,10 +2,26 @@
  * 
  * @param {string} name 
  */
-export const sayHello = name => {
-    if (!name) {
-        return "Hello, World"
-    }
+// export const sayHello = (name,surname) => {
+//     if(!name && !surname) {
+//         return "Hello, World"
+//     }
+//     if (name && !surname) {
+//         return `Hello, ${name}`
+//     }
+//     if (!name && surname) {
+//         return `Hello, ${surname}`
+//     }
+//     if(name && surname) {
+//         return `Hello, ${name} ${surname}`
+//     }   
+// }
 
-    return `Hello, ${name}`
-}
+export const sayHello = (name, surname) => {
+    const greeting = name && surname ? `Hello, ${name} ${surname}` :
+                     name ? `Hello, ${name}` :
+                     surname ? `Hello, ${surname}` :
+                     "Hello, World";
+
+    return greeting;
+};
--- js/samples/unit/unit1.test.js
+++ js/samples/unit/unit1.test.js
@@ -1,11 +1,23 @@
 import { sayHello } from './unit1'
 
 describe('Hello test', () => {
-    it('should return "Hello, World"', () => {
+    it(' should return "Hello, World" ', () => {
+        //no arg
         expect(sayHello()).toBe("Hello, World")
     })
     
-    it('should be "Hello, Thomas"', () => {
-        expect(sayHello("Thomas")).toBe("Hello, Thomas")
+    it(' should be "Hello, Gerard" ', () => {
+        //arg = Gerard
+        expect(sayHello("Gerard")).toBe("Hello, Gerard")
     })
+    it(' should be "Hello, jamin" ', () => {
+        //arg = jamin
+        expect(sayHello("jamin")).toBe("Hello, jamin")
+    })
+
+    it(' should be "Hello, Gerard jamin" ', () => {
+        //arg = Gerard jamin
+        expect(sayHello("Gerard jamin")).toBe("Hello, Gerard jamin")
+    })
+
 })
