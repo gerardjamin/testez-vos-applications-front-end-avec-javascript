8dbc3ce23ca6e69df09536378dfd9d3b11ecfb2c
jamin <gerard.jamin@orange.fr>
2024-01-16 14:59:07 +0100

    delete 1 test

--- js/quiz/question.test.js
+++ js/quiz/question.test.js
@@ -14,10 +14,4 @@ describe('RenderQuestion Integration Test Suites', () => {
             getByTestId(renderQuestion(), 'question-test')
         ).toBeDefined()
     })
-
-    it('should have Hello, Question?', () => {
-        expect(
-            getByTestId(hello-question, 'question-test')
-        ).toBeDefined()
-    })
 })
